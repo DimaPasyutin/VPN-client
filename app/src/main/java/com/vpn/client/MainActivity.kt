616ae4d7f028ea768f4f1546cd8e4d607f1a58a1
DimaPasyutin <dima.pasyutin2@gmail.com>
2023-04-23 13:46:01 +0300

    add vpn connect fragment

--- app/src/main/java/com/vpn/client/MainActivity.kt
+++ app/src/main/java/com/vpn/client/MainActivity.kt
@@ -2,10 +2,13 @@ package com.vpn.client
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vpn.client.ui.vpn_connect.VpnConnectFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().add(R.id.main_graph, VpnConnectFragment(), null)
+            .addToBackStack(null).commit()
     }
 }
---
+++ app/src/main/java/com/vpn/client/ui/vpn_connect/VpnConnectFragment.kt
@@ -0,0 +1,12 @@
+package com.vpn.client.ui.vpn_connect
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.vpn.client.R
+import com.vpn.client.databinding.FragmentVpnConnectBinding
+
+class VpnConnectFragment : Fragment(R.layout.fragment_vpn_connect) {
+
+    private val binding: FragmentVpnConnectBinding by viewBinding()
+
+}
